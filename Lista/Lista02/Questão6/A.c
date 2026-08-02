b335e610aca77a927a36fe7e1cec3cd22d431adc
miguelds-prime <miguel.dimitrius.silva@escola.pr.gov.br>
2026-08-02 21:00:58 +0000

    1 e 6 feitas

--- Lista/Lista02/Questão6/A.c
+++ Lista/Lista02/Questão6/A.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 
 int somaVetor(int v[], int n) {
-    if (n == 0) {
+    // Caso-base: vetor vazio
+    if (n <= 0) {
         return 0;
-    } // Aqui está o Caso Base
-    return v[0] + somaVetor(v + 1, n - 1); 
+    }
+    
+    return v[0] + somaVetor(v + 1, n - 1);
 }
 
 int main() {
 
     int a;
-    printf("Quantos números são?\n");
+
+    printf("Digite a quantidade de números: ");
     scanf("%d", &a);
 
+    if (a <= 0) {
+        printf("A quantidade de elementos deve ser maior que zero.\n");
+    }
+
     int v[a];
-    int soma = 0;
 
-    printf("Digite os numeros inteiros:\n");
-    
+    printf("Digite os %d numeros do vetor:\n", a);
+    for (int i = 0; i < a; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &v[i]);
+    }
 
-    printf("\nResultado:\n");
-    printf("Soma recursiva: %d\n", soma);
+    int r = somaVetor(v, a);
+    printf("\nA soma de todos os elementos e: %d\n", r);
+
+    return 0;
 }
-
-//inacabada
