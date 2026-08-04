29e50aa5d28cb898551d5b3badfd05ae968ca8b3
miguelds-prime <miguel.dimitrius.silva@escola.pr.gov.br>
2026-08-04 18:43:30 +0000

    3B finalizada

--- Lista/Lista02/Questão3/B.c
+++ Lista/Lista02/Questão3/B.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
 int ContarChar (char s[50], int a, char c){
+    int contagem = 0;
 
-    int contagem=0;
-    while(s[a]!=c){
-        if (s[a]= c)
-        contagem=contagem+1;
+    while(s[a] != '\0'){
+        if (s[a] == c) {
+            contagem = contagem + 1;
+        }
+        a++;
     }
 
-    if(contagem==0){
-        return 0;
-    }
-
-    return c;
+    return contagem;
 }
 
+
 int main (){
+
     char pal[50];
-    printf("qual a palavra?");
+    printf("qual a palavra? ");
     scanf("%s", pal);
 
     char l;
-    printf("qual a letra");
-    scanf("%s", l);
+    printf("qual a letra? ");
+    scanf(" %c", &l);
 
-    
+    printf("A letra '%c' aparece %d vezes em \"%s\".\n", l, ContarChar(pal, 0, l), pal);
+
 }
